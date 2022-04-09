2481302c11f46170a3dee12ea5121fa2f8923322
diptowebhero <diptodas1672@gmail.com>
2022-04-09 21:10:24 +0600

    added modal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
+// for accordion
 const accordion = document.getElementsByClassName("content-box");
 for(i=0; i<accordion.length; i++){
     accordion[i].addEventListener("click",function(){
         this.classList.toggle("active")
     })
 }
+// for modal
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const videoBtn = document.querySelectorAll(".video_btn");
+
+overlay.onclick = function(){
+    modal.style.display = "none";
+};
+[...videoBtn].forEach(btn=>{
+    btn.addEventListener("click",function(){
+        modal.style.display = "block";
+    })
+})
